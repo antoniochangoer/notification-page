45d803c5b1a55bc0b1987275d878bb853e98bb08
antoniochangoer <54006126+antoniochangoer@users.noreply.github.com>
2023-10-04 13:01:43 +0200

    project init

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'main-red': '#F65552',
+        'main-blue': '#0A327B',
+      },
+      colors: {
+        'main-red': '#F65552',
+        'main-blue': '#0A327B',
+        'very-dark-grey-blue': '#1C202B',
+        'dark-grey-blue': '#5E6778',
+        'grey-blue': '#939CAD',
+        'light-grey-blue': '#E5EFFA',
+        'very-light-grey-blue': '#DDE7EE',
+        'main-snow': '#F7FAFD',
+      },
+      fontFamily: {
+        main: 'Plus Jakarta Sans, sans-serif',
+      },
+    },
   },
   plugins: [],
 };
